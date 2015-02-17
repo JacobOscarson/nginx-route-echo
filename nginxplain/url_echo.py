1cf83a15ba75e854976c301625f9901ef8f5b484
Jacob Oscarson <jacob@plexical.com>
2015-02-17 15:48:50 +0100

    A very quick-n-dirty URL request echo server

---
+++ nginxplain/url_echo.py
@@ -0,0 +1,15 @@
+import os
+from flask import request, Flask
+
+app = Flask('URL echo server')
+app.debug = True
+
+url_echo = lambda: request.url + os.linesep
+
+@app.route('/')
+def root():
+    return url_echo()
+
+@app.route('/<path:path>')
+def subpath(path):
+    return url_echo()
